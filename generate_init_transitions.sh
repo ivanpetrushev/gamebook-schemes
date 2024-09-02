41f0b50d0e8ecd973166f21f9c244342945ed743
Ivan Petrushev <ivanatora@gmail.com>
2024-09-02 20:09:05 +0300

    bash coding standards

--- generate_init_transitions.sh
+++ generate_init_transitions.sh
@@ -2,13 +2,13 @@
 BEGIN=$1
 END=$2
 
-if [ -z "$BEGIN" ] || [ -z "$END" ]; then
-    echo "Usage: $0 [BEGIN] [END}"
+if [ -z "${BEGIN}" ] || [ -z "${END}" ]; then
+    echo "Usage: $0 [BEGIN] [END]"
     echo "For example: $0 1 50"
     exit 1
 fi
 
 # echo all numbers from $BEGIN to $END
-for i in $(seq $BEGIN $END); do
-    echo "    $i -> "
+for i in $(seq "${BEGIN}" "${END}"); do
+    echo "    ${i} -> "
 done
